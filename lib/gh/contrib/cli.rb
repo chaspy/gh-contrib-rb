6a8048ec2011bf32386f29edd5482f5d93845c8b
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-08 15:06:45 +0900

    Add validation

--- lib/gh/contrib/cli.rb
+++ lib/gh/contrib/cli.rb
@@ -7,6 +7,20 @@ class CLI < Thor
   option :start, type: :string, aliases: '-s', desc: 'Start date to get your contribution'
   option :end, type: :string, aliases: '-e', desc: 'End date to get your contribution'
   def report
+    # validate arguments
+    u = options[:user]
+    s = options[:start]
+    e = options[:end]
+    if u.empty?
+      return false
+    elsif !s.match(/\d{4}-\d{2}-\d{2}/)
+      return false
+    elsif !e.match(/\d{4}-\d{2}-\d{2}/)
+      return false
+    else
+      true
+    end
+
     puts "hello, #{options[:user]}. Get your contribution from #{options[:start]} to #{options[:end]}"
   end
 end
