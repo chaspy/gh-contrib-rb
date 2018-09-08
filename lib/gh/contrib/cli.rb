72ac9d4d6282b62e3c1970dc86de22907b092078
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-08 14:22:54 +0900

    Add start and end option
    Run: `bundle exec exe/gh-contrib report -u chaspy -s 2018-08-31 -e 2018-09-05`
    Put: hello, chaspy. Get your contribution from 2018-08-31 to 2018-09-05

--- lib/gh/contrib/cli.rb
+++ lib/gh/contrib/cli.rb
@@ -4,7 +4,9 @@ require "thor"
 class CLI < Thor
   desc "report your contribution","-u <user>"
   option :user, type: :string, aliases: '-u', desc: 'GitHub user name'
+  option :start, type: :string, aliases: '-s', desc: 'Start date to get your contribution'
+  option :end, type: :string, aliases: '-e', desc: 'End date to get your contribution'
   def report
-    puts "hello, #{options[:user]}"
+    puts "hello, #{options[:user]}. Get your contribution from #{options[:start]} to #{options[:end]}"
   end
 end
