5c650a53f3c35752c9d856467e2d85df09aa96ae
kondo takeshi <chaspy@users.noreply.github.com>
2018-09-10 23:46:37 +0900

    Add test for help

--- spec/gh/contrib_spec.rb
+++ spec/gh/contrib_spec.rb
@@ -2,5 +2,11 @@ RSpec.describe Gh::Contrib do
   it "has a version number" do
     expect(Gh::Contrib::VERSION).not_to be nil
   end
+end
 
+RSpec.describe 'gh-contrib command', type: :aruba do
+  context 'help option' do
+    before(:each) { run('gh-contrib help') }
+    it { expect(last_command_started).to be_successfully_executed }
+  end
 end
